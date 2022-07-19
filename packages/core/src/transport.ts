1ef7ff0f58a3acfa5ac0b898090f7a75d475385d
sjisntsuperman <weishijiestein@outlook.com>
2022-07-20 05:33:36 +0800

    feat: fix: fix package name

--- packages/core/src/transport.ts
+++ packages/core/src/transport.ts
@@ -6,3 +6,9 @@ class Transport {
     const dsn = '';
   }
 }
+
+export function sendReport(data: any) {
+  const beacon = window.navigator.sendBeacon;
+  // todo: 若不支持 beacon 申请降级 xhr
+  return beacon(data);
+}
