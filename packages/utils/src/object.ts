5274e539c3216691be2b53fff24d99482ba3d583
sjisntsuperman <weishijiestein@outlook.com>
2022-08-18 06:14:23 +0800

    feat: feature jest

--- packages/utils/src/object.ts
+++ packages/utils/src/object.ts
@@ -6,15 +6,20 @@ export function getGlobalObject<T>(): T {
   return (isNodeEnv() ? global : typeof window !== 'undefined' ? window : fallbackGlobalObject) as T;
 }
 
-export function fill(global, key, cb) {
+export function fill(global: any, key: string, cb: (data: any) => any) {
   const origin = global[key] as () => void;
   const wrapped = cb(origin);
   if (typeof origin == 'function') {
     try {
-      wrapFunc(origin, wrapped);
+      wrapFunc(wrapped, origin);
     } catch (error) {
       // _sendAndCapture(error);
     }
   }
   global[key] = wrapped;
+}
+
+export function wrapFunc(wrapped: any, original: any) {
+  const proto = original.prototype || {};
+  wrapped.prototype = original.prototype = proto;
 }
---
+++ packages/utils/test/object.test.ts
@@ -0,0 +1,14 @@
+import { getGlobalObject } from '../src/object';
+
+describe('get global', () => {
+  test('should return global object', () => {
+    const backup = global.process;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    delete global.process;
+    const a = getGlobalObject();
+    const b = getGlobalObject();
+    expect(a).toEqual(b);
+    global.process = backup;
+  });
+});
